758040e190deb9933d5a98f9f7bb4deaf87831dc
gyasifred <gyasifred@gmail.com>
2024-02-14 15:20:24 +0000

    *args,**kwargd

--- models/base_model.py
+++ models/base_model.py
@@ -1,46 +1,65 @@
 #!/usr/bin/env python3
-import uuid
+from uuid import uuid4
 from datetime import datetime
-
-"""Define a class BaseModel
-
-Public instance attributes:
-    id: string - assign with a uuid when an instance is created
-        you can use uuid.uuid4() to generate a unique id, but don’t forget to convert to a string
-        the goal is to have a unique id for each BaseModel
-    created_at: datetime - assign with the current datetime when an instance is created
-    updated_at: datetime - assign with the current datetime when an instance is created and updated every time you change your object
-
-Public instance methods:
-    save(self): updates the public instance attribute updated_at with the current datetime
-    to_dict(self): returns a dictionary containing all keys/values of __dict__ of the instance
-        by using self.__dict__, only instance attributes set will be returned
-        a key __class__ must be added to this dictionary with the class name of the object
-        created_at and updated_at must be converted to a string object in ISO format:
-            format: %Y-%m-%dT%H:%M:%S.%f (ex: 2017-06-14T22:31:03.285259)
-            you can use isoformat() of the datetime object
-    __str__(self): should print: [<class name>] (<self.id>) <self.__dict__>
-
-This class will be the base for other classes, providing common attributes and methods.
-"""
 
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    """Base class for model objects, providing common attributes and methods for managing identity, timestamps, and basic serialization."""
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes a BaseModel instance.
+
+        Args:
+            *args: Optional positional arguments (ignored).
+            **kwargs: Optional keyword arguments to set instance attributes.
+                    Special keys:
+                        - 'created_at' (str/datetime): Timestamp of creation.
+                        - 'updated_at' (str/datetime): Timestamp of last update.
+                        - Other keys will set corresponding instance attributes.
+
+        Raises:
+            ValueError: If a non-datetime value is provided for 'created_at' or 'updated_at'.
+        """
+        for key, value in kwargs.items():
+            if key == "__class__":
+                continue
+
+            if key in {"created_at", "updated_at"}:
+                if not isinstance(value, (str, datetime)):
+                    raise ValueError(
+                        f"Invalid value for '{key}'. Expected a datetime string or object.")
+                value = datetime.strptime(
+                    value, "%Y-%m-%dT%H:%M:%S.%f") if isinstance(value, str) else value
+
+            setattr(self, key, value)
+
+        if "id" not in kwargs:
+            self.id = str(uuid4())
+
+        if "created_at" not in kwargs:
+            self.created_at = datetime.now()
+
+        if "updated_at" not in kwargs:
+            self.updated_at = datetime.now()
 
     def save(self):
+        """Updates the 'updated_at' attribute to the current timestamp."""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """Returns a dictionary representation of the instance.
+
+        Returns:
+            dict: A dictionary representing the instance's attributes.
+                  Includes class name, ID, and formatted timestamps.
+        """
         cls_name = self.__class__.__name__
-        obj_dict = self.__dict__.copy()
+        obj_dict = {key: value.isoformat() if isinstance(value, datetime) else value
+                    for key, value in self.__dict__.items()}
         obj_dict['__class__'] = cls_name
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """Returns a human-readable string representation of the instance."""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
