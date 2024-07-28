9131ea46953848a36bd6ccbf1c4371216dd63895
BalindaPacifique <paccybalinda@gmail.com>
2024-07-28 13:08:40 -0700

    balinda

--- models/base_model.py
+++ models/base_model.py
@@ -2,23 +2,30 @@
 
 import uuid
 from datetime import datetime
-#from models1 import storage
 
 class BaseModel:
     """ This class will define all public instances attributes and models"""
+
     def __init__(self, *args,**kwargs):
         """this module defines these public instances attributes"""
+
         self.id = str(uuid.uuid4)
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
     def __str__(self):
         """ this module defines to doc of the class BaseModel"""
+
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
     def save(self):
         """This module save and updates the process"""
+
         self.updated_at = datetime.now()
+
     def to_dict(self):
         """Defines a dictionary which save all the public instances"""
+
         new_dict = self.__dict__.copy()
         new_dict["class"] = self.__class__.__name__
         new_dict["created_at"] = self.created_at.isoformat()
