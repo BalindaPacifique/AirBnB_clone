b4494d0cc39f1a6489346b27dfcdcbc60e0a032b
BalindaPacifique <paccybalinda@gmail.com>
2024-07-30 08:52:49 -0700

    now we can build our basemodel from a dictionay

---
+++ tests/test_models/models/base_model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """ This class will define all public instances attributes and models"""
+
+    def __init__(self, *args,**kwargs):
+        """this module defines these public instances attributes"""
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at":
+                    value =  datetime.fromisoformat(kwargs["created_at"])
+                if key == "updated_at":
+                    value = datetime.fromisoformat(kwargs["updated_at"])
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4)
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """ this module defines to doc of the class BaseModel"""
+
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """This module save and updates the process"""
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Defines a dictionary which save all the public instances"""
+
+        new_dict = self.__dict__.copy()
+        new_dict["class"] = self.__class__.__name__
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+
+        return new_dict
