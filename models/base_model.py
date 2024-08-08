73acb5f15b66b5f3c68cd6923d9bc902c79117ee
BalindaPacifique <141261588+BalindaPacifique@users.noreply.github.com>
2024-08-08 19:23:50 +0200

    I need these images

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 import uuid
 from datetime import datetime
 """from engine.file_storag import FileStorage"""
-from __init__ import storage
+from models import storage
 
 class BaseModel:
     """ This class will define all public instances attributes and models"""
@@ -22,6 +22,7 @@ class BaseModel:
             self.id = str(uuid.uuid4)
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """ this module defines to doc of the class BaseModel"""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 import json
-from base_model import BaseModel
-from __init__ import storage
+from models.base_model import BaseModel
 
 class FileStorage:
     """ This class will help us to serialize and deserialize our dictionaries"""
@@ -20,11 +19,12 @@ class FileStorage:
         self.__objects[key] = obj
     def save(self):
         """This module serialize the __objecs dict to json file"""
-
+        serialized_objects = {}
         for key, obj in self.__objects.items():
-            with open(self.__file_path, "w") as file:
-                json.dump(self.__objects, file)
+            serialized_objects[key] = obj.to_dict()
+        with open(self.__file_path, "w") as file:
+            json.dump(serialized_objects, file)
     def reload(self):
         """This module defines the load module"""
         with open(self.__file_path, "r") as file:
-            seflf.__object = json.load(file)
+            self.__object = json.load(file)
